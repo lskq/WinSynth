577d17a3d389e01f925ad1771311830e8c47c6dc
lskq <lskq012025@gmail.com>
2025-04-28 14:56:01 +0200

    Add piano keys to interface

--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -2,13 +2,106 @@ namespace WinSynth.Forms;
 
 public class MainForm : Form
 {
+    public int KeyWidth => ClientSize.Width / 7;
+    public int KeyHeight => ClientSize.Height;
+    public Panel Piano = new();
+
     public MainForm()
     {
         InitializeComponent();
     }
 
+    override protected void OnResize(EventArgs e)
+    {
+        ResizePiano();
+    }
+
     public void InitializeComponent()
     {
         Text = "WinSynth";
+        MinimumSize = new Size(800, 400);
+
+        InitializePiano();
+
+        OnResize(EventArgs.Empty);
+    }
+
+    public void ResizePiano()
+    {
+        Piano.Size = new Size(ClientSize.Width, ClientSize.Height);
+
+        foreach (var control in Piano.Controls)
+        {
+            var button = (Button)control;
+
+            if (button.Name.Length == 1)
+            {
+                button.Width = KeyWidth;
+                button.Height = KeyHeight;
+                button.Font = new Font("", KeyHeight / 10);
+            }
+            else
+            {
+                button.Width = KeyWidth / 2;
+                button.Height = KeyHeight * 3 / 4;
+                button.Font = new Font("", KeyHeight / 20);
+            }
+
+            button.Location = button.Name switch
+            {
+                "C" => new Point(0, 0),
+                "C#" => new Point(KeyWidth * 3 / 4, 0),
+                "D" => new Point(KeyWidth, 0),
+                "D#" => new Point(KeyWidth * 7 / 4, 0),
+                "E" => new Point(KeyWidth * 2, 0),
+                "F" => new Point(KeyWidth * 3, 0),
+                "F#" => new Point(KeyWidth * 15 / 4, 0),
+                "G" => new Point(KeyWidth * 4, 0),
+                "G#" => new Point(KeyWidth * 19 / 4, 0),
+                "A" => new Point(KeyWidth * 5, 0),
+                "A#" => new Point(KeyWidth * 23 / 4, 0),
+                "B" => new Point(KeyWidth * 6, 0),
+                _ => throw new Exception()
+            };
+        }
+    }
+
+    public void InitializePiano()
+    {
+        Piano.Dock = DockStyle.Fill;
+
+        Button[] blackKeys = [];
+        string[] labels = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+        for (int i = 0; i < 12; i++)
+        {
+            var button = new Button
+            {
+                TabIndex = i,
+                Name = labels[i],
+                Text = labels[i],
+                TextAlign = ContentAlignment.BottomCenter,
+            };
+
+            if (button.Name.Length == 1)
+            {
+                button.BackColor = Color.FromArgb(255, 255, 255);
+                button.ForeColor = Color.FromArgb(15, 15, 15);
+            }
+            else
+            {
+                button.BackColor = Color.FromArgb(15, 15, 15);
+                button.ForeColor = Color.FromArgb(255, 255, 255);
+                blackKeys = [.. blackKeys, button];
+            }
+
+            Piano.Controls.Add(button);
+        }
+
+        foreach (var key in blackKeys)
+        {
+            key.BringToFront();
+        }
+
+        Controls.Add(Piano);
     }
 }
